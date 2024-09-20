ca52c32beef0295a80490d130adf5debc0c60dcb
Vidhi Sharma <vidhi.sharma@MacBook-Pro-2.local>
2024-09-20 16:27:43 +0530

    setup linitin

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Welcome from './welcome';
+
+function App() {
+
+   console.log('yes')
+  return (
+    <div className="App">
+      <Welcome/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/welcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <div>
+      <h1>Welcome to my React App</h1>
+      
+    </div>
+  )
+}
+
+export default Welcome
